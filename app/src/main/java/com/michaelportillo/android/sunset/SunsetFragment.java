72f39b68bcceceae0e926012f0f91402889c5807
USER <portillom@g.cofc.edu>
2019-01-17 20:50:37 -0800

    Providing ArgbEvaluator

--- app/src/main/java/com/michaelportillo/android/sunset/SunsetFragment.java
+++ app/src/main/java/com/michaelportillo/android/sunset/SunsetFragment.java
@@ -1,5 +1,6 @@
 package com.michaelportillo.android.sunset;
 
+import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
 import android.content.res.Resources;
 import android.os.Bundle;
@@ -62,6 +63,7 @@ public class SunsetFragment extends Fragment {
         ObjectAnimator sunsetSkyAnimator = ObjectAnimator
                 .ofInt(mSkyView, "backgroundColor", mBlueSkyColor, mSunsetSkyColor)
                 .setDuration(3000);
+        sunsetSkyAnimator.setEvaluator(new ArgbEvaluator());
 
         heightAnimator.start();
         sunsetSkyAnimator.start();
